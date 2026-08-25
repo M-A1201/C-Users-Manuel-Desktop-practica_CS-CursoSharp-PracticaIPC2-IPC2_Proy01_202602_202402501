2138e6c5a6e00bea1a0bcb8689193508630c883d
M-A1201 <2946755580408@ingenieria.usac.edu.gt>
2026-08-24 22:12:35 -0600

    completar la clase ListaSimple con metodo buscar y eliminar, y validar los TDAs

--- Proyecto01/Program.cs
+++ Proyecto01/Program.cs
@@ -5,14 +5,23 @@ class Program
     {
       ListaSimple lista1 = new ListaSimple();
 
-            // Insertamos datos de prueba
             lista1.Agregar("Guatemala");
             lista1.Agregar("Escuintla");
             lista1.Agregar("Quetzaltenango");
 
-            // Imprimimos el contenido y el contador
-            Console.WriteLine("--- Elementos en la Lista ---");
+            Console.WriteLine("--- Lista Inicial ---");
             lista1.Mostrar();
 
-            Console.WriteLine("\nTotal de elementos: " + lista1.Contador);  }
+            // Prueba de Búsqueda
+            Console.WriteLine("\n¿Existe Escuintla?: " + lista1.Buscar("Escuintla"));
+            Console.WriteLine("¿Existe Petén?: " + lista1.Buscar("Petén"));
+
+            // Prueba de Eliminación
+            Console.WriteLine("\nEliminando 'Escuintla'...");
+            lista1.Eliminar("Escuintla");
+
+            Console.WriteLine("\n--- Lista Tras Eliminar ---");
+            lista1.Mostrar();
+            Console.WriteLine("Total de elementos: " + lista1.Contador);
+         }
 }
