bd0518ee7740911d04634f8a5b8e6a1511bc6a2c
M-A1201 <2946755580408@ingenieria.usac.edu.gt>
2026-08-24 21:52:51 -0600

    crear clase listaSimple con atributos privados, propiedaes y metodo

---
+++ Proyecto01/ListaSimple.cs
@@ -0,0 +1,17 @@
+class ListaSimple
+    {
+        // Atributos privados de la clase Lista Simple
+        private Nodo cabeza;
+        private int contador;
+
+        // Propiedades públicas para encapsulamiento
+        public Nodo Cabeza
+        {
+            get { return cabeza; }
+        }
+//metodo contador
+        public int Contador
+        {
+            get { return contador; }
+        }
+    }
